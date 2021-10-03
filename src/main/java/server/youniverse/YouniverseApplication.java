c7f5e530dfe6a0e3840447a7455f6ce2a2e13f08
anzun0 <zxzimin@naver.com>
2021-10-03 22:44:57 +0900

    initial commit :)

---
+++ src/main/java/server/youniverse/YouniverseApplication.java
@@ -0,0 +1,13 @@
+package server.youniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouniverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/youniverse/YouniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package server.youniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

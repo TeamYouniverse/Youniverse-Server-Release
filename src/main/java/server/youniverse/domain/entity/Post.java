f93ab345689f3983d7c3659fccbccfb041818ec2
anzun0 <zxzimin@naver.com>
2021-10-07 02:11:41 +0900

    add: BaseEntity 클래스 별도 분리

--- src/main/java/server/youniverse/domain/entity/Post.java
+++ src/main/java/server/youniverse/domain/entity/Post.java
@@ -2,6 +2,7 @@ package server.youniverse.domain.entity;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -14,10 +15,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import server.youniverse.domain.entity.Planet;
 
 @Entity
-//@Table(name="Boards")
 @Getter
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
+@NoArgsConstructor
+public class Post extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="post_id")
@@ -28,16 +28,9 @@ public class Post {
 
     @Enumerated(EnumType.STRING)
     private Planet emotion;
+
     private boolean active;
 
-    @CreatedDate
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    private LocalDateTime updatedDate;
-
-    public Post() {
-
-    }
     public Post(@NotBlank String nickname,@NotBlank String contents, @NotBlank Planet emotion){
         this.nickname=nickname;
         this.contents=contents;

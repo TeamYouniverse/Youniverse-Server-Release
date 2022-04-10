25550a3260c2cf7dc025143e19660da4807bb507
soleu <dlthf555@gmail.com>
2022-04-11 00:43:00 +0900

    [FIX] jwt 토큰 발급 방식 수정

---
+++ src/main/java/server/youniverse/controller/dto/TokenRequestDto.java
@@ -0,0 +1,11 @@
+package server.youniverse.controller.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenRequestDto {
+    private String accessToken;
+    private String refreshToken;
+}
